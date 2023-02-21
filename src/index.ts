307a7ba2238528d32ee9a3ca6c63420f305f2032
Delavalom <laarvelo@icloud.com>
2023-02-20 21:27:48 -0400

    update packages and tokens limit

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,37 @@ import { brotliCompressSync } from "zlib";
 
 config();
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-const token = process.env.TELEGRAM_BOT_TOKEN
-const openai = new OpenAIApi(configuration)
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const openai = new OpenAIApi(configuration);
 
-invariant(token, "Couldn't read the token the enviroment variable")
+invariant(token, "Couldn't read the token the enviroment variable");
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", async (msg) => {
-    const baseCompletion = await openai.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `${msg.text}.\n`,
-        temperature: 0.8,
-        max_tokens: 1000,
-    })
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${msg.text}.\n`,
+    temperature: 0.8,
+    max_tokens: 4000,
+  });
 
-    const chatId = msg.chat.id
+  const chatId = msg.chat.id;
 
-    bot.sendMessage(chatId, "please wait a few seconds")
+  bot.sendMessage(chatId, "please wait a few seconds");
 
-    const basePromptOuput = baseCompletion.data.choices.pop()
+  const basePromptOuput = baseCompletion.data.choices.pop();
 
-    if(!basePromptOuput?.text) {
-        return bot.sendMessage(chatId, "please try again, AI couldn't send the data")
-    }
+  if (!basePromptOuput?.text) {
+    return bot.sendMessage(
+      chatId,
+      "please try again, AI couldn't send the data"
+    );
+  }
 
-    bot.sendMessage(chatId, basePromptOuput?.text)
-})
+  bot.sendMessage(chatId, basePromptOuput?.text);
+});
 
-bot.on("error", (err) => console.log(err))
+bot.on("error", (err) => console.log(err));
