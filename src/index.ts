2385b9be6599cfb126071cae6cacd1f81608f094
Luis Angel Arvelo <laarvelo@icloud.com>
2023-08-13 16:28:46 +0000

    refactor entry file

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,12 @@
-import { Messages, generate } from "~/lib/openai.js";
-import { initializeWhatsapp, whatsapp } from "~/lib/whatsapp.js";
-import { redisMethods } from "~/lib/redis.js";
-import { telegram } from "~/lib/telegram.js";
+import { handleTelegramMessage, telegram } from "~/lib/telegram.js";
+import { handleWhatsappMessage, initializeWhatsapp, whatsapp } from "~/lib/whatsapp.js";
+import { redisMethods } from "./lib/redis.js";
+import { generate } from "./lib/openai.js";
 
-telegram.on("message", async (msg) => {
-  if (!msg.text) return;
-  const { id } = msg.chat;
-
-  const { get, set } = await redisMethods();
-
-  const messages = await get(id);
-
-  if (!messages) {
-    const initialGeneration = [
-      { role: "system", content: "You are a helpful assistant" },
-      { role: "user", content: msg.text },
-    ] satisfies Messages;
-    const generation = await generate(initialGeneration);
-    telegram.sendMessage(id, generation.message);
-    set(id, [
-      ...initialGeneration,
-      { role: "assistant", content: generation.message },
-    ]);
-    return;
-  }
-
-  messages.push({ role: "user", content: msg.text });
-  const generation = await generate(messages);
-
-  telegram.sendMessage(id, generation.message);
-
-  messages.push({ role: "assistant", content: generation.message });
-  set(id, messages);
-});
+telegram.on("message", (msg) => handleTelegramMessage(msg, redisMethods, generate))
 
 telegram.on("error", (err) => console.log(err.message));
 
-// _________________________________________________________________whatsapp under this lines
-
-
 initializeWhatsapp()
 
-// whatsapp messages only trigger ai generation by the prefix of "bot:"
-whatsapp.on("message", async (msg) => {
-  if (!msg.body.toLowerCase().startsWith("b:")) return;
-
-  const { id } = msg.id;
-
-  const { get, set } = await redisMethods();
-
-  const messages = await get(id);
-
-  if (!messages) {
-    const initialGeneration = [
-      { role: "system", content: "You are a helpful assistant" },
-      { role: "user", content: msg.body.replace("bot:", "") },
-    ] satisfies Messages;
-    const generation = await generate(initialGeneration);
-    msg.reply(generation.message);
-    set(id, [
-      ...initialGeneration,
-      { role: "assistant", content: generation.message },
-    ]);
-    return;
-  }
-
-  messages.push({ role: "user", content: msg.body.replace("bot:", "") });
-  const generation = await generate(messages);
-
-  msg.reply(generation.message);
-
-  messages.push({ role: "assistant", content: generation.message });
-  set(id, messages);
-});
+whatsapp.on("message", (msg) => handleWhatsappMessage(msg, redisMethods, generate))
