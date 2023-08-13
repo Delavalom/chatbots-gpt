d4137f7dc9b2ffecc26719d7103bec130319f0d0
Luis Angel Arvelo <laarvelo@icloud.com>
2023-08-13 16:26:28 +0000

    refactor whatsapp integration

--- src/lib/whatsapp.ts
+++ src/lib/whatsapp.ts
@@ -1,22 +1,61 @@
 import ws from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
+import WAWebJS from "whatsapp-web.js";
+import { RedisMethods } from "./redis.js";
+import { Generate, Messages } from "./openai.js";
 
 export const whatsapp = new ws.Client({
-  authStrategy: new ws.LocalAuth()
+  authStrategy: new ws.LocalAuth(),
 });
 
 export function initializeWhatsapp() {
   whatsapp.on("qr", (qr) => {
     qrcode.generate(qr, { small: true });
   });
-  
+
   whatsapp.on("remote_session_saved", () => {
     console.log("Remote Session stored!");
   });
-  
+
   whatsapp.on("ready", () => {
     console.log("Whatsapp client is ready!");
   });
-  
+
   whatsapp.initialize();
+}
+
+export async function handleWhatsappMessage(
+  msg: WAWebJS.Message,
+  redisMethods: RedisMethods,
+  generate: Generate
+) {
+  if (!msg.body.toLowerCase().startsWith("b:")) return;
+
+  const { id } = msg.id;
+
+  const { get, set } = await redisMethods();
+
+  const messages = await get(id);
+
+  if (!messages) {
+    const initialGeneration = [
+      { role: "system", content: "You are a helpful assistant" },
+      { role: "user", content: msg.body.replace("bot:", "") },
+    ] satisfies Messages;
+    const generation = await generate(initialGeneration);
+    msg.reply(generation.message);
+    set(id, [
+      ...initialGeneration,
+      { role: "assistant", content: generation.message },
+    ]);
+    return;
+  }
+
+  messages.push({ role: "user", content: msg.body.replace("bot:", "") });
+  const generation = await generate(messages);
+
+  msg.reply(generation.message);
+
+  messages.push({ role: "assistant", content: generation.message });
+  set(id, messages);
 }
