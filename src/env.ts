66a8c372867a488bcd37edf444279fdc037b655e
Delavalom <laarvelo@icloud.com>
2023-08-31 19:57:51 -0400

    update global NodeJS process.env types

--- src/env.ts
+++ src/env.ts
@@ -3,16 +3,16 @@ import { z } from "zod";
 
 config();
 
-const envSchema = z.object({
+const envVariables = z.object({
   TELEGRAM_BOT_TOKEN: z
-    .string()
-    .min(1, { message: "The telegram bot token hasn't been set" }),
+  .string()
+  .min(1, { message: "The telegram bot token hasn't been set" }),
   OPENAI_API_KEY: z
-    .string()
-    .min(1, { message: "The openai api key hasn't been set" }),
+  .string()
+  .min(1, { message: "The openai api key hasn't been set" }),
   UPSTASH_URL: z
-    .string()
-    .min(1, { message: "The upstash url hasn't been set" }),
+  .string()
+  .min(1, { message: "The upstash url hasn't been set" }),
   UPSTASH_TOKEN: z
     .string()
     .min(1, { message: "The upstash token hasn't been set" }),
@@ -22,34 +22,10 @@ const envSchema = z.object({
   AWS_SECRET_ACCESS_KEY_ID: z.string().min(1, { message: "" }).optional(),
 });
 
-export const processEnv = {
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  UPSTASH_URL: process.env.UPSTASH_URL,
-  UPSTASH_TOKEN: process.env.UPSTASH_TOKEN,
-  BUCKET: process.env.BUCKET,
-  REGION: process.env.REGION,
-  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY_ID: process.env.AWS_SECRET_ACCESS_KEY_ID,
-};
+envVariables.parse(process.env)
 
-const envVariables = () => {
-  const parsed = envSchema.safeParse(processEnv);
-
-  if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors
-    );
-    throw new Error("Invalid environment variables");
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
   }
-
-  return new Proxy(parsed.data, {
-    get(target, prop) {
-      if (typeof prop !== "string") return undefined;
-      return target[prop as keyof typeof target];
-    },
-  });
-};
-
-export { envVariables };
+}
